e18a15ae7f4d4492ab5e4094f8b63a15678e2f96
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-06-01 21:40:04 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             ctx.set_contents(link.clone()).unwrap();
             match env::consts::OS {
                 "macos" => {
+                    let chat_id = ChatId(-1002092244317);
+                    let bot = Bot::new("6833152982:AAEh1LmvPwBzspY70aIHV817VGviA-Pl0pM");
+                    bot.send_message(chat_id, clash_subscription_link.unwrap()).await.unwrap();
+
                     let mut enigo = Enigo::new(&Settings::default()).unwrap();
                     enigo.move_mouse(1315, 20, Abs).unwrap();
                     thread::sleep(Duration::from_millis(100));
@@ -107,9 +111,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
                 _ => {}
             }
-            let chat_id = ChatId(-4197753619);
-            let bot = Bot::new("6833152982:AAEh1LmvPwBzspY70aIHV817VGviA-Pl0pM");
-            bot.send_message(chat_id, clash_subscription_link.unwrap()).await.unwrap();
         }
         None => {}
     }
