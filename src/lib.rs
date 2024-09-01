8d6e255739dcab1b28905fabdfd404483ed0c910
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-09-01 14:36:07 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod api;
 pub mod util;
 pub mod cyan;
 pub mod mail_tm;
-pub mod panda;
 pub mod gou;
 pub mod xfx_ssr;
 pub mod wall;
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use fern::Dispatch;
 use log::LevelFilter;
 use teloxide::{prelude::Requester, types::ChatId, Bot};
 
-use clash_tool::{cyan, gou, mail_tm, panda, util, wall, xfx_ssr};
+use clash_tool::{cyan, gou, mail_tm, util, wall, xfx_ssr};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -41,14 +41,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             clash_subscription_link = Option::from(subscription_link.unwrap().to_string());
         }
         "2" => {
-            log::info!("You chose to register a Panda account.");
-            let temp_email_account = mail_tm::create_temp_mail_account().await?;
-            panda::verify(&temp_email_account.address).await?;
-            let verification_code = mail_tm::get_verification_code(&temp_email_account).await?;
-            panda::register(&temp_email_account, verification_code).await?;
-            clash_subscription_link = Option::from(panda::login(&temp_email_account).await?);
-        }
-        "3" => {
             log::info!("You chose to register a 加速狗 account.");
             let temp_email_account = mail_tm::create_temp_mail_account().await?;
             gou::send_verification_code_to_email(&temp_email_account.address).await?;
@@ -57,13 +49,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let cookies = gou::login(&temp_email_account).await?;
             clash_subscription_link = Option::from(gou::get_subscription_link(&cookies).await?);
         }
-        "4" => {
+        "3" => {
             log::info!("You chose to register a 小飞侠SSR account.");
             let random_email_account = util::get_random_email(&util::get_random_username(8, 10)).to_string();
             xfx_ssr::register(&random_email_account).await?;
             clash_subscription_link = Option::from(xfx_ssr::login(&random_email_account).await?);
         }
-        "5" => {
+        "4" => {
             log::info!("You chose to register a 墙了个墙 account.");
             let temp_email_account = mail_tm::create_temp_mail_account().await?;
             wall::send_verification_code_to_email(&temp_email_account.address).await?;
@@ -72,7 +64,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let cookies = wall::login(&temp_email_account).await?;
             clash_subscription_link = Option::from(wall::get_subscription_link(&cookies).await?);
         }
-        "h" => { print!("1: 青森\n2: Panda\n3: 加速狗\n4: 小飞侠SSR\n5: 墙了个墙\nh: show help\n"); }
+        "h" => { print!("1: 青森\n2: 加速狗\n3: 小飞侠SSR\n4: 墙了个墙\nh: show help\n"); }
         _ => { println!("doing nothing"); }
     }
     match clash_subscription_link {
