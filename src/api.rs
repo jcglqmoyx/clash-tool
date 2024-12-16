8f2f6a7931e39d30dbc84deafd47ef4b079f1b5f
Dennis Ritchie <jcglqmoyx@gmail.com>
2024-12-16 18:20:25 +0800

    update

--- src/api.rs
+++ src/api.rs
@@ -12,12 +12,6 @@ pub mod gou {
     pub const USER_PROFILE_API: &str = "https://jiasugou.me/user";
 }
 
-pub mod xfx_ssr {
-    pub const REGISTRATION_API: &str = "https://xfxssr.top/api/v1/passport/auth/register";
-    pub const LOGIN_API: &str = "https://xfxssr.top/api/v1/passport/auth/login";
-    pub const SUBSCRIPTION_LINK_PREFIX: &str = "http://xfxssr.info/api/v1/client/subscribe?token=";
-}
-
 pub mod wall {
     pub const MAIL_VERIFICATION_CODE_API: &str = "https://www.qlgq.top/auth/send";
     pub const REGISTRATION_API: &str = "https://www.qlgq.top/auth/register";
